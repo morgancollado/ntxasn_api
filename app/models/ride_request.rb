12e588856163c88234eeef3be9335ada4c3b7ca3
morgan collado <morgan.collado@gmail.com>
2021-06-20 17:54:06 -0500

    sets enums for ride request

--- app/models/ride_request.rb
+++ app/models/ride_request.rb
@@ -1,6 +1,6 @@
 class RideRequest < ApplicationRecord
     has_many :rides 
     belongs_to :passenger
-    enum :status {active: 0, inactive: 1, completed: 2}
+    enum :status {assigned: 0, unassigned: 1, canceled: 2}
 
 end 
